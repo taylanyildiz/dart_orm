a12903a21f0c68ad9ffe1a14e5ac913be9f17c02
Taylan YILDIZ <taylanyildz@outlook.com>
2023-08-26 16:50:12 +0300

    package updated

--- bin/flutter_orm.dart
+++ bin/flutter_orm.dart
@@ -1,14 +1,17 @@
+import 'dart:io' show Directory;
+import 'package:path/path.dart' as path;
 import 'dart:io';
 
 void main(List<String> args) {
+  final dir = path.join(Directory.current.path, "include");
   Process.runSync(
     'cmake',
     ['.'],
-    workingDirectory: "include/",
+    workingDirectory: dir,
   );
   Process.runSync(
     'make',
     [],
-    workingDirectory: "include/",
+    workingDirectory: dir,
   );
 }
